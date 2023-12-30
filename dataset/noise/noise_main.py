296bafcd1807916076951984d250d699564e44f3
domser2002 <domser2002@o2.pl>
2023-12-31 00:44:49 +0100

    necessary includes in main

--- dataset/noise/noise_main.py
+++ dataset/noise/noise_main.py
@@ -1,5 +1,9 @@
 from GaussianNoise import *
 from SaltAndPepper import *
+from PoissonNoise import *
+from SpeckleNoise import *
+from UniformNoise import *
+from QuantizationNoise import *
 
 gaussian_noise("/home/domser/studia/semestr5/ML/dlib/repo/dataset/noise/test.jpg",
                "/home/domser/studia/semestr5/ML/dlib/repo/dataset/noise/Gaussian.jpg")
