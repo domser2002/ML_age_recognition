a0a7375a3fd21fb81c1a41ffc3d8ea6f85f77f0d
domser2002 <domser2002@o2.pl>
2023-12-31 00:43:30 +0100

    Quantization Noise

---
+++ dataset/noise/QuantizationNoise.py
@@ -0,0 +1,12 @@
+import cv2
+import numpy as np
+
+def quantization_noise(path, output_path, levels=64):
+    img = cv2.imread(path)
+    if img is None:
+        print(f"Error: Unable to load the image at {path}")
+        return
+    quantization_levels = 256 // levels
+    noisy_img = (img / quantization_levels) * quantization_levels
+    cv2.imwrite(output_path, noisy_img)
+    
