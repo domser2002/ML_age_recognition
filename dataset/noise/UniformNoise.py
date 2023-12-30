5cc1abb18039069632950d5b001c51745c176dce
domser2002 <domser2002@o2.pl>
2023-12-31 00:48:57 +0100

    finish main

--- dataset/noise/UniformNoise.py
+++ dataset/noise/UniformNoise.py
@@ -1,11 +1,13 @@
 import cv2
 import numpy as np
 
-def speckle_noise(path,output_path, variance=0.02):
+def uniform_noise(path, output_path, low=0, high=50):
     img = cv2.imread(path)
     if img is None:
         print(f"Error: Unable to load the image at {path}")
         return
-    noise = np.random.normal(0, variance, img.shape)
-    noisy_img = img + img * noise
+    noise = np.random.uniform(low, high, img.shape).astype(np.uint8)
+    noisy_img = cv2.add(img, noise)
     cv2.imwrite(output_path, noisy_img)
+    print(f"Noisy image saved at: {output_path}")
+
--- dataset/noise/noise_main.py
+++ dataset/noise/noise_main.py
@@ -9,3 +9,11 @@ gaussian_noise("/home/domser/studia/semestr5/ML/dlib/repo/dataset/noise/test.jpg
                "/home/domser/studia/semestr5/ML/dlib/repo/dataset/noise/Gaussian.jpg")
 salt_and_pepper_noise("/home/domser/studia/semestr5/ML/dlib/repo/dataset/noise/test.jpg",
                "/home/domser/studia/semestr5/ML/dlib/repo/dataset/noise/SaltAndPepper.jpg")
+poisson_noise("/home/domser/studia/semestr5/ML/dlib/repo/dataset/noise/test.jpg",
+               "/home/domser/studia/semestr5/ML/dlib/repo/dataset/noise/Poisson.jpg")
+speckle_noise("/home/domser/studia/semestr5/ML/dlib/repo/dataset/noise/test.jpg",
+               "/home/domser/studia/semestr5/ML/dlib/repo/dataset/noise/Speckle.jpg")
+uniform_noise("/home/domser/studia/semestr5/ML/dlib/repo/dataset/noise/test.jpg",
+               "/home/domser/studia/semestr5/ML/dlib/repo/dataset/noise/Uniform.jpg")
+quantization_noise("/home/domser/studia/semestr5/ML/dlib/repo/dataset/noise/test.jpg",
+               "/home/domser/studia/semestr5/ML/dlib/repo/dataset/noise/Quantization.jpg")
