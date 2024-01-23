033eed6dcfb81d61d6485559abe6845de44c534c
Dominik Seredyn <86359641+domser2002@users.noreply.github.com>
2024-01-23 14:18:19 +0100

    Age detection (#1)
    * inception-net
    
    * minor notebook changes
    
    * first onnx model
    
    * age detection script update
    
    * training notebook update
    
    ---------
    
    Co-authored-by: dawidsroczyk <dawid.sroczyk54@gmail.com>

--- age_detection.py
+++ age_detection.py
@@ -1,54 +1,27 @@
 import os
-import torch
-import torch.nn as nn
-import torch.nn.parallel
-import torch.utils.data
-import torchvision.datasets as dset
-import torchvision.transforms as transforms
-import torchvision.utils as vutils
-from torch.utils.data import Dataset, DataLoader, random_split
-import cv2
+import onnx
+import numpy as np
 from PIL import Image
-from torchvision import datasets, models
-from torchvision.models import ResNet50_Weights
+import onnxruntime as rt
+from torchvision import transforms
 
 class AgeDetector:
-    def __init__(self, path_to_weights):
-        self.image_size=224
+
+    def __init__(self, onnx_path = None):
+        if onnx_path is None:
+            onnx_path = os.path.join('age_detection', 'age_models', 'inception3.onnx')
+
+        self.sess = rt.InferenceSession(onnx_path, providers=rt.get_available_providers())
         self.transform = transforms.Compose([
-            transforms.Resize((self.image_size, self.image_size)),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.4914, 0.4822, 0.4465],
-                std=[0.2023, 0.1994, 0.2010],)
+            transforms.Resize((224, 224)),
+            transforms.ToTensor()
         ])
-        self.device = torch.device("cpu")
-        self.model = models.resnet50(weights=ResNet50_Weights.IMAGENET1K_V2).to(self.device)
+        self.input_name = self.sess.get_inputs()[0].name
+        self.output_name = self.sess.get_outputs()[0].name
 
-        for param in self.model.parameters():
-            param.requires_grad = False
-        self.model.fc = nn.Sequential(
-            nn.Linear(2048, 512),
-            nn.ReLU(inplace=True),
-            
-            nn.Linear(512, 512),
-            nn.ReLU(inplace=True),
-            
-            nn.Linear(512, 512),
-            nn.ReLU(inplace=True),
-            
-            nn.Linear(512, 1)
-        ).to(self.device)
-        self.model = torch.load(path_to_weights, map_location=self.device)
-
-    def detect_age(self, test_img):
-        test_img = Image.fromarray(test_img, mode='RGB')
-        test_img = self.transform(test_img).to(self.device).detach().reshape((1, 3, self.image_size, self.image_size))
-        age = float(self.model(test_img))
-        age = round(age)
-        return age
-
-    def test_detector(self, img_path):
-        print(self.detect_age(cv2.imread(img_path)))
-
-detector = AgeDetector('/home/domser/studia/semestr5/ML/dlib/repo/age_detection/age_models/resnet_5.pt')
-detector.test_detector('/home/domser/studia/semestr5/ML/dlib/repo/age_detection/lenna.png')
+    def detect_age(self, img: np.array):
+        PIL_img = Image.fromarray(img, mode='RGB')
+        img_transform = self.transform(PIL_img).numpy().reshape((1, 3, 224, 224))
+        res = self.sess.run([self.output_name], {self.input_name: img_transform})
+        res = res[0][0][0]
+        return res
