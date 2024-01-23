d778e3586d7839c1d7f74bedb44ae369f8cc18d4
domser2002 <domser2002@o2.pl>
2024-01-23 01:37:47 +0100

    handle absolute and relative paths

--- test/test_directory.py
+++ test/test_directory.py
@@ -7,4 +7,11 @@ print("- for each image txt file with correct result must be provided")
 print("- naming convension must be like in documentation")
 print("Enter a directory path (relative or absolute):")
 path = input()
+
+if os.path.isabs(path):
+    print("Absolute path provided.")
+else:
+    path = os.path.abspath(path)
+    print(f"Converted relative path to absolute path: {path}")
+
 print(path)
