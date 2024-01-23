a290f5361fe9e8f554965dcf40930dfe91ab85bc
domser2002 <domser2002@o2.pl>
2024-01-23 01:32:30 +0100

    print basic info

---
+++ test/test_directory.py
@@ -0,0 +1,10 @@
+import os
+
+print("********TEST AGE DETECTION ON DIRECTORY**********")
+print("Requirments:")
+print("- directory must contain jpg images")
+print("- for each image txt file with correct result must be provided")
+print("- naming convension must be like in documentation")
+print("Enter a directory path (relative or absolute):")
+path = input()
+print(path)
